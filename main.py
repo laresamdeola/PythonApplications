f376d256ece0bcb622f2afce8b904dff5e756022
laresamdeola <laresamdeola@gmail.com>
2024-01-14 08:31:50 +0000

    add save functionality for website, email and password

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website}|{email}|{password}| \n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+        email_entry.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -27,15 +38,17 @@ password_label.grid(row=3, column=0)
 #Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "youremail@gmail.com")
 password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1, columnspan=2)
 
 #Buttons
 password_button = Button(text="Generate Password", width=30)
 password_button.grid(row=4, column=1, columnspan=2)
-add_button = Button(text="Add", width=30)
+add_button = Button(text="Add", width=30, command=save)
 add_button.grid(row=5, column=1, columnspan=2)
 
 window.mainloop()
