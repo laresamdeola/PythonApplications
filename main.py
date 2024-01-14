9a853e8fbe01839e0137da2b7ced3d3b9eb02c28
laresamdeola <laresamdeola@gmail.com>
2024-01-14 06:29:11 +0000

    gui for the password manager python app

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("The Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(window, height=200, width=200)
+logo_img = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+#canvas.pack()
+
+#Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+#Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=35)
+password_entry.grid(row=3, column=1, columnspan=2)
+
+#Buttons
+password_button = Button(text="Generate Password", width=30)
+password_button.grid(row=4, column=1, columnspan=2)
+add_button = Button(text="Add", width=30)
+add_button.grid(row=5, column=1, columnspan=2)
+
+window.mainloop()
+
+
+
+
+
