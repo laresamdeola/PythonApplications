22b2f4ffd700882c5063527583d636b6999a4cb3
laresamdeola <laresamdeola@gmail.com>
2024-02-17 11:29:42 +0000

    add sheety api configuration

--- NutritionApp/main.py
+++ NutritionApp/main.py
@@ -1,5 +1,9 @@
 from config import APP_ID, API_KEY
 import requests
+from datetime import datetime
+
+
+time = datetime.now()
 
 API_ENDPOINT = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
@@ -21,4 +25,36 @@ options = {
 response = requests.post(url=options['url'], headers=options['headers'], json=nutrition_parameters)
 response.raise_for_status()
 nutrition_data = response.json()
-print(f'You have burnt {nutrition_data['exercises'][0]['nf_calories']} calories.')
+# print(nutrition_data)
+# print(nutrition_data['exercises'][0]['name'])
+# print(nutrition_data['exercises'][0]['duration_min'])
+# print(nutrition_data['exercises'][0]['nf_calories'])
+
+sheety_endpoint = 'https://api.sheety.co/d875da4dd9bf749bb9fc338f4efe3afd/myWorkouts2/sheet1'
+
+# 1tyqNcY1K_PYdNIG_B1S4j2OXC1PkmaevmIzY8HdY_Ko
+
+for exercise in nutrition_data['exercises']:
+    body = {
+        'sheet1': {
+            'date': time.strftime('%d/%m/%Y'),
+            'time': time.strftime('%T'),
+            'exercise': exercise['name'].title(),
+            'duration': exercise['duration_min'],
+            'calories': exercise['nf_calories']
+        }
+    }
+
+    sheet_response = requests.post(sheety_endpoint, json=body)
+    print(sheet_response.text)
+
+'''
+sheety_response = requests.get(sheety_endpoint)
+sheety_response.raise_for_status()
+sheety_data = sheety_response.json()
+print(sheety_data)
+'''
+
+
+
+
