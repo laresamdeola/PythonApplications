05533a3169eccddf4ba4ce1700ebbba5f018223a
laresamdeola <laresamdeola@gmail.com>
2024-03-03 16:26:10 +0000

    Python Decorators 2

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -3,10 +3,29 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper_function():
+        return f'<b>{function()}</b>'
+    return wrapper_function
+
+
+def text_emphasis(function):
+    def wrapper_function():
+        return f'<em>{function()}</em>'
+    return wrapper_function
+
+
 @app.route('/')
+@make_bold
+@text_emphasis
 def home():
     return 'Hello World!'
 
 
+@app.route('/username/<year>')
+def happy_new_year(year):
+    return f'Welcome to year {year} number'
+
+
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
