73a34613479c1728641990aa9b18b066b2598955
laresamdeola <laresamdeola@gmail.com>
2024-05-14 05:33:06 +0100

    Flask Web Application.

--- ContactForm/server.py
+++ ContactForm/server.py
@@ -13,16 +13,35 @@ def home_page():
     return render_template('index.html')
 
 
+@app.route('/base')
+def base_page():
+    return render_template('base.html')
+
+
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
+
+@app.route('/success')
+def success_page():
+    return render_template('success.html')
+
+
+@app.route('/denied')
+def denied_page():
+    return render_template('denied.html')
 
 
 @app.route('/login', methods=['POST'])
 def login():
     name = req.form['name_input']
     password = req.form['password_input']
-    return render_template('login.html', name=name, password=password)
+    if name == 'admin@email.com' and password == '12345678':
+        return render_template('success.html')
+    else:
+        return render_template('denied.html')
+    # return render_template('login.html', name=name, password=password)
 
 
 if __name__ == '__main__':
